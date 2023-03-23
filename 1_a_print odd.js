df6beef203e50ac68b7691994afefd780dccf2ff
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-23 13:49:15 +0530

    Create 1_a_print odd.js
    Print odd numbers in an array using anonymous function

---
+++ 1_a_print odd.js
@@ -0,0 +1,18 @@
+let odd = (function(arr)
+{
+    let odd =[];
+    let j = 0;
+    for(var i =0; i<arr.length; i++)
+        {
+            var num = +arr[i];
+            var mod = num%2;
+            if(mod != 0)
+            {
+             odd[j] = arr[i];
+             j++
+            }
+        }
+    return odd;
+})
+let at = [1,2,3,4,5,6,7,8,9]
+console.log(odd(at));
