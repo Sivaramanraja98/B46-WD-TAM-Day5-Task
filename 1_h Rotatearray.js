780a7c51368c40069d1b09cab0892e0f93038fde
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-23 14:13:27 +0530

    Create 1_h Rotatearray.js
    Rotate an array by k times using anonymous function

---
+++ 1_h Rotatearray.js
@@ -0,0 +1,10 @@
+let rotatearray = function (arr, k) {
+    k = k % arr.length;
+    A = arr.slice(k);
+    B = arr.slice(0, k);
+    arr = A.concat(B);
+    return arr;
+  }
+  let arr = ['1','2','3','4','5','6']
+  let k = 3
+  console.log(rotatearray(arr,k))
