2954a5ee1b4512afc760a76d0dca06b320f927f2
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-23 20:49:05 +0530

    Create 1_b_titlecaps IIFE.js
    Convert all the strings to title caps in a string array IIFE

---
+++ 1_b_titlecaps IIFE.js
@@ -0,0 +1,18 @@
+arr = ['hi','hello','siva']
+let titlecaps = (function(arr)
+{
+var newarr = [];
+var j = 0;
+for(var i=0; i<arr.length; i++)
+{
+A = arr[i].split('');
+B = A[0].toUpperCase()
+b=A.slice(1,A.length);
+c = b.unshift(B);
+D =b.join('');
+newarr[j]= D;
+j++
+}
+return newarr;
+})(arr);
+console.log(titlecaps);
