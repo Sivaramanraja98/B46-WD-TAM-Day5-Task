eee01ef6f17bf1a43dcc31337fd5b0016c5d6b77
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-23 20:59:20 +0530

    Create 1_f_median of array IIFE.js
    Return median of two sorted arrays of the same size using IIFE

---
+++ 1_f_median of array IIFE.js
@@ -0,0 +1,14 @@
+let a = [23,45,12];
+let b = [17,25,18];
+let median = (function (arr1,arr2)
+{
+    let arr = [...arr1,...arr2];
+    let arrsort = arr.sort();
+    let arrlen = arrsort.length
+    c = arrlen/2;
+    c1 = c-1;
+    let media = (arrsort[c]+arrsort[c1])/2;
+return media
+})(a,b);
+
+console.log(median);
