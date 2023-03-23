32cff41aa05d595d9ba2bb5061df4084f8d1a58f
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-23 21:19:33 +0530

    Create 2_e_palindrome Arrow.js
    Return all the palindromes in an array using Arrow

---
+++ 2_e_palindrome Arrow.js
@@ -0,0 +1,19 @@
+const palindromearr = arr => {
+  let palindrome = [];
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let arrsp = arr[i].split("");
+    let arrrev = arrsp.reverse();
+    let arrjoin = arrrev.join("");
+    if (arr[i] == arrjoin) {
+      palindrome[j] = arr[i];
+      j++;
+    }
+  }
+
+  return palindrome;
+}
+
+const arr = ['454', '595', '696', '123', 'odd', 'radar'];
+console.log(palindromearr(arr));
